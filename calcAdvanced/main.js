56c7bdceb073cfa13b165bec42be734bc8b093f1
Amir-Mangisto <Mangisto.man>
2021-09-30 20:12:22 +0300

    update

--- calcAdvanced/main.js
+++ calcAdvanced/main.js
@@ -1,21 +1,16 @@
-// const numberButtons=document.getElementsByClassName("number");
-// const operatorButtons=document.getElementsByClassName("operator");
-// const equal=document.getElementById("equal");
-// const delet=document.getElementById("delet");
-// const clearAll=document.getElementById("clearAll");
-// const previous=document.getElementById("previous-operand");
-// const current=document.getElementById("current-operand");
+const numberButtons=document.getElementsByClassName("number");
+const operatorButtons=document.getElementsByClassName("operator");
+const equal=document.getElementById("equal");
+const delet=document.getElementById("delet");
+const clearAll=document.getElementById("clearAll");
+const previous=document.getElementById("previous-operand");
+const current=document.getElementById("current-operand");
 
 
 
-
-// let operatorButtons=document.getElementsByClassName("operator");
-// let numberButtons=document.getElementsByClassName("number");
-// let clear=document.getElementById("clear");
-
-// numberButtons.onclick= function(){
-//     alert("jkhgf");
-// }
+numberButtons.onclick= function(){
+    alert("jkhgf");
+}
 
 
 
