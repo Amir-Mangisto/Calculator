76127b6c2bb8026dca70dfab6c6b85d836374d11
Amir-Mangisto <Mangisto.man>
2021-09-30 20:02:45 +0300

    first commit

---
+++ calcAdvanced/main.js
@@ -0,0 +1,23 @@
+// const numberButtons=document.getElementsByClassName("number");
+// const operatorButtons=document.getElementsByClassName("operator");
+// const equal=document.getElementById("equal");
+// const delet=document.getElementById("delet");
+// const clearAll=document.getElementById("clearAll");
+// const previous=document.getElementById("previous-operand");
+// const current=document.getElementById("current-operand");
+
+
+
+
+// let operatorButtons=document.getElementsByClassName("operator");
+// let numberButtons=document.getElementsByClassName("number");
+// let clear=document.getElementById("clear");
+
+// numberButtons.onclick= function(){
+//     alert("jkhgf");
+// }
+
+
+
+
+
